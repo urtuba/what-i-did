2857c3c16caa83c2854b70575a3c9e9d5ba39e61
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2022-01-12 10:48:43 +0300

    Implement command line input

--- wid.js
+++ wid.js
@@ -1,16 +1,38 @@
 import database from './db/database.js'
+import readline from 'readline'
 import Log from './model/log.js'
 
-if (import.meta.url == `file://${process.argv[1]}`) {
 
-  if(process.argv[2] == 'log') {
-    const tags = process.argv.slice(3)
-    const log = new Log("Hello World", tags)
-    database.saveLog(log)
-  }
+const log = async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
   
-  else if(process.argv[2] == 'ls') {
-    console.log(database.getLogs())
-  }
-  
+  const prompt = (query) => new Promise(resolve => rl.question(query, resolve))
+
+  const note = await prompt('Note: ')
+  const tags = process.argv.slice(3)
+  const log = new Log(note, tags)
+
+  database.saveLog(log)
+
+  rl.close()
 }
+
+const list = async (tags = null) => {
+  const logs = database.getLogs()
+  console.log(logs)
+}
+
+const main = async () => {
+  if(process.argv[2] == 'log')
+    log()
+  
+  else if(process.argv[2] == 'ls')
+    console.log(database.getLogs())
+
+}
+
+if (import.meta.url == `file://${process.argv[1]}`)
+  main()
