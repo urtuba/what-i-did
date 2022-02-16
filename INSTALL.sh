f0f8ba52869746fef0b75ea0e625dd3d028d99ed
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2022-02-16 13:36:09 +0300

    Introduce INSTALL.sh

---
+++ INSTALL.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo 'WHAT-I-DID'
+echo 'Personal Logger'
+
+cp -rf ./what-i-did ~/.what-i-did
+echo '-----> files copied'
+
+npm --prefix ~/.what-i-did install ~/.what-i-did
+echo '-----> dependencies installed'
+
+echo "alias wid='node ~/.what-i-did/wid.js'" >> ~/.zshrc
+source ~/.zshrc
+alias wid2='node ~/.what-i-did/wid.js'
+echo '-----> terminal command installed'
+
+echo 'DONE'
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # what-i-did
- Simple Daily Logging
+
+Simple Personal Logger CLI
+
+## How to install
+
+### zsh Terminals
+
+Clone the repository to any location.
+```
+git clone https://github.com/urtuba/what-i-did.git
+```
+
+Enter to the directory.
+```
+cd what-i-did
+```
+
+Install
+```
+zsh INSTALL.sh
+```
